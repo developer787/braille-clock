4bd86bc25b3bb87df663dd9ec36e3d3a00e97866
Alfredo Colon <developer787@users.noreply.github.com>
2023-08-20 11:52:19 +0000

    main push

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
 import { useEffect } from 'react';
 
 export default function Home() {
-  const brailleCharacters = [
-    '⠁', '⠃', '⠉', '⠙', '⠑', '⠋', '⠛', '⠓', '⠊', '⠚', // Braille digits 1-0
-    '⠼⠁', '⠼⠃', '⠼⠉', '⠼⠙', '⠼⠑', '⠼⠋', '⠼⠛', '⠼⠓', '⠼⠊', '⠼⠚', // Braille digits 11-20
-    // Add more braille characters as needed
-  ];
+  // const brailleCharacters = [
+  //   '⠁', '⠃', '⠉', '⠙', '⠑', '⠋', '⠛', '⠓', '⠊', '⠚', // Braille digits 1-0
+  //   '⠼⠁', '⠼⠃', '⠼⠉', '⠼⠙', '⠼⠑', '⠼⠋', '⠼⠛', '⠼⠓', '⠼⠊', '⠼⠚', // Braille digits 11-20
+  //   // Add more braille characters as needed
+  // ];
 
-  const handleTouchStart = (brailleChar) => {
-    // Find the index of the braille character
-    const charIndex = brailleCharacters.findIndex(char => char === brailleChar);
-    if (charIndex !== -1) {
-      // Vibrate for 100ms
-      navigator.vibrate(100);
-    }
-  };
+  // const handleTouchStart = (brailleChar) => {
+  //   // Find the index of the braille character
+  //   const charIndex = brailleCharacters.findIndex(char => char === brailleChar);
+  //   if (charIndex !== -1) {
+  //     // Vibrate for 100ms
+  //     navigator.vibrate(100);
+  //   }
+  // };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
